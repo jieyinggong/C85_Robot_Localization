324b1b4619240b9bbadfba34ce1bd7dbaf4ee752
jieyingong <jieying.gong@mail.utoronto.ca>
2025-10-22 19:57:10 -0400

    add turn at intersection and update drive along

--- motion.h
+++ motion.h
@@ -15,3 +15,19 @@ int turn_left_90_degrees(void);
 int turn_back_180_degrees(void);
 
 #endif
+#ifndef INTERSECTION_H
+#define INTERSECTION_H
+
+#include <stdbool.h>
+#include <math.h>
+#include "calibration.h"
+
+int scan_intersection(int *tl, int *tr, int *br, int *bl);
+int leftright_turn_degrees(int direction, double target_angle);
+int detect_intersection(void);
+
+int turn_right_90_degrees(void);
+int turn_left_90_degrees(void);
+int turn_back_180_degrees(void);
+
+#endif
