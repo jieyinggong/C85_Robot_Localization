4de1769d60059cd61f6cf3c211945c4078192848
jieyingong <jieying.gong@mail.utoronto.ca>
2025-10-22 16:27:01 -0400

    add const.h

---
+++ const.h
@@ -0,0 +1,22 @@
+#ifndef CONST_H
+#define CONST_H
+
+#define DIR_UP    0
+#define DIR_RIGHT 1
+#define DIR_DOWN  2
+#define DIR_LEFT  3
+
+
+#define C_BLACK  0
+#define C_WHITE  1
+#define C_RED    2
+#define C_YELLOW 3
+#define C_GREEN  4
+#define C_BLUE   5
+
+#define COLOR_PORT PORT_1
+#define GYRO_PORT PORT_2
+#define LEFT_MOTOR MOTOR_A
+#define RIGHT_MOTOR MOTOR_D
+
+#endif // CONST_H
